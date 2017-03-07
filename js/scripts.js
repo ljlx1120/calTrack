bc359fc9a1949337c581b167f1718f8df8050b9a
Eliot Carlsen and Xing Li <eliot+xing@>
2017-03-07 10:01:23 -0800

    add new Activities and front end functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
-
-
-Biking 8.0
-Rock-climbing 9.5
-Hiking 6.0
-Running 7.5
-Walking 3.8
-Yoga 3.0
-Skiing, Snowboarding 7.0
-Watching sports 1.5
-Grocery shopping 2.10
-Taking Epicodus class 2.17
-
-
 function Activity (participatedActivity, metValue) {
   this.participatedActivity = participatedActivity;
   this.metValue = metValue;
-}
+};
+
+var biking = new Activity ("biking", 8.0);
+var rockclimbing = new Activity ("rockclimbing", 9.5);
+var hiking = new Activity ("hiking", 6.0);
+var running = new Activity ("running", 7.5);
+var walking = new Activity ("walking", 3.8);
+var yoga = new Activity ("yoga", 3.0);
+var skiing = new Activity ("skiing", 7.0);
+var snowboarding = new Activity ("snowboarding", 7.0);
+var watchingSports = new Activity ("watching sports", 1.5);
+var groceryShopping = new Activity ("grocery shopping", 2.10);
+var takingEpicodusClass = new Activity ("taking epicodus class", 2.17);
+
+
+
 
 Activity.prototype.calCalculator = function() {
   var caloriesBurned = this.metValue * weightInKilos * hoursOfExcercise;
@@ -32,4 +33,13 @@ function convertPounds(pounds) {
   return weightInKilos;
 };
 
-$(document)
+
+
+
+$(document).ready(function(){
+  $().submit(function(event){
+    event.preventDefault();
+
+
+  });
+});
