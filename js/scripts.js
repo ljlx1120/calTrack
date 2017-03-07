945c65fe884a2776ef4b804ff8c44036b2d193d6
Eliot Carlsen and Xing Li <eliot+xing@>
2017-03-07 09:09:04 -0800

    add new specs based on calculations

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+Step 1
+
+Convert your weight in pounds to kilograms by dividing by 2.2. For example, if you weigh 176 pounds then you weigh 80 kilograms.
+
+Step 2
+Look up the MET value of the activity that you performed. These figures are available from the websites of the National Cancer Institute and the Institute of Lifestyle Medicine as well as in the Compendium of Physical Activities Tracking Guide, which is available on the website of the University of South Carolina Arnold School of Public Health. For example, general cycling has a MET value of 8.0.
+
+Sponsored Links
+Grammar & Spelling Check
+Makes Sure Everything You Type Is Easy To Read And Mistake-Free. Try Now!
+grammarly.com
+Step 3
+
+Multiply the MET value by your weight in kilograms. If you weigh 80 kilograms and perform an activity like cycling with a MET value of 8.0, for example, this would give you 640 as a product.
+
+Step 4
+
+Multiply the product by the time you performed the activity in hours to get the number of calories you burned. For example, if you cycled for 30 minutes then you would multiply 640 by 0.5 to get 320 calories.
+
+
+Harris Benedict Method
+
+BMR Men: = 66 + (6.23 X weight in pounds) + (12.7 X height in inches) – (6.8 X age)
+
+BMR Women: = 655 + (4.35 X weight in pounds) + (4.7 X height in inches) – (4.7 X age)
+
+Katch & McArdle Method
+
+BMR (Men + Women) = 370 + (21.6 * Lean Mass in kg)
+
+Lean Mass = weight in kg – (weight in kg * body fat %)
+1 kg = 2.2 pounds, so divide your weight by 2.2 to get your weight in kg
+
+Activity Multiplier (Both HB + KA Method use same activity multiplier)
+
+Little or No Exercise, Desk Job	1.2 x BMR
+Light Exercise, Sports 1 to 3 Times Per Week	1.375 x BMR
+Moderate Exercise, Sports 3 to 5 Times Per Week	1.55 x BMR
+Heavy Exercise, Sports 6 to 7 Times Per Week	1.725 x BMR
