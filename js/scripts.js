f695d0973e13ec52815d5d70fee921dc4f5c4794
Eliot Carlsen and Xing Li <eliot+xing@>
2017-03-07 12:15:41 -0800

    add calCalculator functionality with correct outputs of cals burned

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@ function Activity (participatedActivity, metValue, weight, time) {
 
 
 Activity.prototype.calCalculator = function() {
-  var caloriesBurned = this.metValue * weightInKilos * hoursOfExcercise;
+  console.log("calCalculator")
+  var caloriesBurned = this.metValue * this.weight * this.time;
+  console.log(caloriesBurned);
   return caloriesBurned;
 };
 
@@ -18,17 +20,27 @@ Activity.prototype.lbCalculator = function() {
 
 function convertMinToHours(hours, minutes) {
   var hoursOfExcercise = hours + minutes/60;
-  console.log(hoursOfExcercise);
   return hoursOfExcercise;
 };
 
 function convertPounds(pounds) {
   var weightInKilos = pounds * .453592;
-  console.log(weightInKilos);
   return weightInKilos;
 };
 
+function activityChoice (userActivity, weight, time) {
+  if(userActivity === "biking"){
+    console.log("Made it in the if")
+    var biking = new Activity ("biking", 8.0, weight, time);
+    biking.calCalculator();
+    console.log(biking.weight);
+    console.log(biking.time);
+    console.log(biking.calCalculator());
 
+  } else {
+    console.log("at end of activityChoice");
+  };
+};
 
 
 $(document).ready(function(){
@@ -41,20 +53,23 @@ $(document).ready(function(){
     var userActivity = $("#userActivity").val();
     var userTimeHours = parseInt($("#userTimeHours").val());
     var userTimeMinutes = parseInt($("#userTimeMinutes").val());
-    convertPounds(userWeight);
-    convertMinToHours(userTimeHours, userTimeMinutes);
+    var kilos = convertPounds(userWeight);
+    var hours = convertMinToHours(userTimeHours, userTimeMinutes);
+    console.log(kilos);
+    console.log(hours);
+    activityChoice(userActivity, kilos, hours);
 
-    var biking = new Activity ("biking", 8.0,);
-    var rockclimbing = new Activity ("climbing", 9.5);
-    var hiking = new Activity ("hiking", 6.0);
-    var running = new Activity ("running", 7.5);
-    var walking = new Activity ("walking", 3.8);
-    var yoga = new Activity ("yoga", 3.0);
-    var skiing = new Activity ("skiing", 7.0);
-    var snowboarding = new Activity ("snowboarding", 7.0);
-    var watchingSports = new Activity ("watching", 1.5);
-    var groceryShopping = new Activity ("grocery", 2.10);
-    var takingEpicodusClass = new Activity ("epicodus", 2.17);
+
+    // var rockclimbing = new Activity ("climbing", 9.5, weightInKilos, hoursOfExcercise);
+    // var hiking = new Activity ("hiking", 6.0, weightInKilos, hoursOfExcercise);
+    // var running = new Activity ("running", 7.5, weightInKilos, hoursOfExcercise);
+    // var walking = new Activity ("walking", 3.8, weightInKilos, hoursOfExcercise);
+    // var yoga = new Activity ("yoga", 3.0, weightInKilos, hoursOfExcercise);
+    // var skiing = new Activity ("skiing", 7.0, weightInKilos, hoursOfExcercise);
+    // var snowboarding = new Activity ("snowboarding", 7.0, weightInKilos, hoursOfExcercise);
+    // var watchingSports = new Activity ("watching", 1.5, weightInKilos, hoursOfExcercise);
+    // var groceryShopping = new Activity ("grocery", 2.10, weightInKilos, hoursOfExcercise);
+    // var takingEpicodusClass = new Activity ("epicodus", 2.17, weightInKilos, hoursOfExcercise);
 
 
   });
