2691677126dff789afd89d21d6088ca019c46276
Xing Li and Zach Beecher <xing+zach@>
2017-03-09 09:32:46 -0800

    add animation

--- js/scripts.js
+++ js/scripts.js
@@ -27,33 +27,93 @@ function convertPounds(pounds) {
 
 function activityChoice (userActivity, weight, time) {
   if(userActivity === "biking"){
+    $("img#bike").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var biking = new Activity ("biking", 8.0, weight, time);
     return biking.calCalculator();
   } else if (userActivity === "rockclimbing"){
+    $("img#climb").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var rockclimbing = new Activity ("climbing", 9.5, weight, time);
     return rockclimbing.calCalculator();
   } else if (userActivity === "hiking") {
+    $("img#hike").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var hiking = new Activity ("hiking", 6.0, weight, time);
     return hiking.calCalculator();
   } else if (userActivity === "running"){
+    $("img#run").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var running = new Activity ("running", 7.5, weight, time);
     return running.calCalculator();
   } else if (userActivity === "walking"){
+    $("img#walk").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var walking = new Activity ("walking", 3.8, weight, time);
     return walking.calCalculator();
   } else if (userActivity === "yoga"){
+    $("img#yoga").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var yoga = new Activity ("yoga", 3.0, weight, time);
     return yoga.calCalculator();
   } else if (userActivity === "snowboarding"){
+    $("img#snowboard").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var snowboarding = new Activity ("snowboarding", 7.0, weight, time);
     return snowboarding.calCalculator();
   } else if (userActivity === "watching"){
+    $("img#watch").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000, function(){$(this).removeAttr('style');});
     var watchingSports = new Activity ("watching", 1.5, weight, time);
     return watchingSports.calCalculator();
   } else if (userActivity === "grocery"){
+    $("img#grocery").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000);
     var groceryShopping = new Activity ("grocery", 2.10, weight, time);
     return groceryShopping.calCalculator();
   } else if (userActivity === "epicodus"){
+    $("img#epicodus").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '200px',
+            width: '200px'
+        }, 2000);
     var takingEpicodusClass = new Activity ("epicodus", 2.17, weight, time);
     return takingEpicodusClass.calCalculator();
   };
